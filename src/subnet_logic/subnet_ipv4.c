997f18ec24c18294eeea3a9a5e898156edb24684
Pascal Böhler <pascal@bembel.dev>
2023-10-09 08:16:49 +0200

    feat: Added method to calculate network from given IP

--- src/subnet_logic/subnet_ipv4.c
+++ src/subnet_logic/subnet_ipv4.c
@@ -1,7 +1,7 @@
 #include "subnet_ipv4.h"
+#include <sys/_types/_u_int8_t.h>
 
 #define OCTETS_AVAILABLE 4
-#define TESTSUBNET 28
 #define HOSTS_PER_OCTET 256 
 
 short* calculate_decimal_mask(int cidr_id) {
@@ -58,26 +58,51 @@ int calculate_hosts_available(int cidr) {
 void calculate_x_ip_in_network(short * network, int cidr, int xIp) {
     for (int i = 0; i < OCTETS_AVAILABLE; i++) {
         printf("%i\n", network[i]);
+    } 
+}
+
+short * calculate_ip_network_from_mask(short * octets, short * ip) {
+    short * network = (short *) malloc(sizeof(short[OCTETS_AVAILABLE]));
+    for (int i = 0; i < OCTETS_AVAILABLE; i++) {
+
+        short ipPart = ip[i];
+        short octetPart = octets[i];
+
+        printf("%i;%i", ipPart, octetPart);
+
+        short networkPart = (ipPart & octetPart);
+        printf("Debug: %i\n", networkPart);
+        network[i] = networkPart;
     }
-    
+    return network;
 }
 
 int main() {
-    u_int8_t octets[4];
+    short * octets = (short *) malloc(sizeof(short[4]));
     octets[0] = 255;
     octets[1] = 255;
     octets[2] = 255;
     octets[3] = 0;
     int cidr = calculate_cidr_id(octets);
 
-    short network[4];
-    network[0] = 192;
-    network[1] = 168;
-    network[2] = 172;
-    network[3] = 0;
+    short ip[4];
+    ip[0] = 192;
+    ip[1] = 168;
+    ip[2] = 178;
+    ip[3] = 250;
 
+    short * network = calculate_ip_network_from_mask(octets, ip);
+
+    int help = (0 & 128);
+
+    printf("IP: %i\n", help);
+    printf("Network: %i.%i.%i.%i\n", network[0], network[1], network[2], network[3]);
+    printf("Subnetmask: %i.%i.%i.%i\n", octets[0], octets[1], octets[2], octets[3]);
     printf("CIDR Notation: %i\n", cidr);
     printf("Hosts available: %i\n", calculate_hosts_available(cidr));
     
     calculate_x_ip_in_network(network, cidr, 1);
+
+    free(octets);
+    free(network);
 }
